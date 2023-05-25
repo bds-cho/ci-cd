f8c24f0e3809e000c7a642e219d0e52aba275590
bschouhan1029 <130922884+bschouhan1029@users.noreply.github.com>
2023-05-25 23:01:50 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 docker stop $(docker ps -aq) 2>/dev/null
 docker rm $(docker ps -aq) 2>/dev/null
 docker rmi webapp-image 2>/dev/null
-docker build ./workspace/MavenBuild -t webapp-image
+docker build . -t webapp-image
 docker run -d -p 5000:8080 webapp-image
